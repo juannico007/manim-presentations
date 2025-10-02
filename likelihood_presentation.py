f30fb0519bbd00e72de3ff7819d98f802cef792b
juannico007 <juan24092002@gmail.com>
2025-10-02 23:02:26 +0200

    First slide of presentation

--- likelihood_presentation.py
+++ likelihood_presentation.py
@@ -2,6 +2,19 @@ from manim import *
 from manim_slides import Slide
 
 class MinimalPresentation(Slide):
+    def add_common_ancestor(self, graph, labels,  node1, node2):
+        new_vertex = len(graph.vertices)
+        pos1 = graph[node1].get_center()
+        pos2 = graph[node2].get_center()
+        midpoint_x = (pos1[0] + pos2[0]) / 2
+        new_pos = [midpoint_x, max(pos1[1], pos2[1]) + 1, 0]
+        self.play(graph.animate.add_vertices(new_vertex, positions={new_vertex: new_pos}))
+        label = Text('S' + str(new_vertex+1)).scale(0.5)
+        label.next_to(graph[new_vertex], LEFT, buff=0.1)  # position relative to vertex
+        labels.add(label)
+        self.play(FadeIn(label))
+        self.play(graph.animate.add_edges((node1, new_vertex), (node2, new_vertex)))
+
     def construct(self):
         # Slide 1
         title = Text("Maximum likelihood in phylogeny")
@@ -10,10 +23,12 @@ class MinimalPresentation(Slide):
 
         # Slide 2
         self.play(FadeOut(title))
-        title = Text("We want to make a phylogenetic tree for 4 species").to_edge(UP)
+        title = Text("We have 4 species").to_edge(UP)
         self.play(Create(title))
         self.next_slide()
         
+
+
         species_1 = 'AGCCTAG'
         species_2 = 'ATACTGG'
         species_3 = 'GGCTTCG'
@@ -40,11 +55,71 @@ class MinimalPresentation(Slide):
     
         # Add the table to the scene
         self.add(species_table)
-        self.play(Create(species_table))
+        self.play(FadeIn(species_table))
         self.next_slide()
 
-        
+        self.play(species_table.animate.scale(0.75).to_corner(UL, buff=1.5)
+                  )
+        # Get the second column (index 2 because Manim counts from 1)
+        col = species_table.get_columns()[2]
 
+        # Highlight it with a background rectangle
+        highlight = SurroundingRectangle(col, color=YELLOW, buff=0.1)
+        highlight.set_fill(YELLOW, opacity=0.3)
+
+        self.play(Create(highlight))
+        self.next_slide()
+
+        # Get the second column (index 6 because Manim counts from 1)
+        col = species_table.get_columns()[6]
+        # Highlight it with a background rectangle
+        highlight = SurroundingRectangle(col, color=RED, buff=0.1)
+        highlight.set_fill(RED, opacity=0.3)
+
+        self.play(Create(highlight))
+        self.next_slide()
+
+
+
+        vertices = [i for i in range(4)]
+        edges = []
+        graph1_site2 = Graph(vertices, edges)
+        graph1_site2[0].move_to([0, 0, 0])
+        graph1_site2[1].move_to([3, 0, 0])
+        graph1_site2[2].move_to([1, 0, 0])
+        graph1_site2[3].move_to([2, 0, 0])
+
+            
+        graph1_site2.next_to(species_table, RIGHT, buff=1)
+
+        labels = VGroup()
+        for v in vertices:
+            label = Text('S' + str(v+1)).scale(0.5)
+            label.next_to(graph1_site2[v], DOWN, buff=0.1)  # position relative to vertex
+            labels.add(label)
+        graph1 = VGroup(graph1_site2, labels)
+
+        self.play(Create(graph1_site2))
+        self.play(FadeIn(labels))
+        self.next_slide()
+
+        self.add_common_ancestor(graph1_site2, labels,  0, 2)
+        self.play(graph1.animate.shift(DOWN))
+        self.next_slide()
+
+        self.add_common_ancestor(graph1_site2, labels,  3, 4)
+        self.play(graph1.animate.shift(DOWN))
+        self.next_slide()
+
+        self.add_common_ancestor(graph1_site2, labels,  1, 5)
+        self.next_slide()
+
+        highlight_graph1 = SurroundingRectangle(graph1, color=YELLOW, buff=0.1)
+        highlight_graph1.set_fill(YELLOW, opacity=0.3)
+
+        self.play(Create(highlight_graph1))
+        self.next_slide()
+        
         # vertices = [i for i in range(11)]
         # edges = []
         # g = Graph(vertices, edges)
